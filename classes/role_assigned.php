5c39c2f1ec427d0a4fedd4350d2129d0602e25d0
Andrew Zito <zitoa@lafayette.edu>
2018-11-05 13:53:57 -0500

    Initial commit (generated with tool_plugin_skeleton_generator)

---
+++ classes/role_assigned.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin observer classes are defined here.
+ *
+ * @package     mod_wordpress_sync
+ * @category    event
+ * @copyright   2018 Lafayette College
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_wordpress_sync;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event observer class.
+ *
+ * @package    mod_wordpress_sync
+ * @copyright  2018 Lafayette College
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class role_assigned {
+
+    /**
+     * Triggered via $event.
+     *
+     * @param \core\event\role_assigned $event The event.
+     * @return bool True on success.
+     */
+    public static function role_assigned($event) {
+
+        // For more information about the Events API, please visit:
+        // https://docs.moodle.org/dev/Event_2
+
+        return true;
+    }
+}
---
+++ lang/en/wordpress_sync.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_wordpress_sync
+ * @category    string
+ * @copyright   2018 Lafayette College
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'newmodule';
+$string['modulename_help'] = 'Use the WordPress Sync module to let teachers provision WordPress sites automatically';
+$string['modulenameplural'] = 'wordpress_syncs';
+$string['newmodulefieldset'] = 'Custom example fieldset.';
+$string['newmodulename'] = 'newmodule name';
+$string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field';
+$string['newmodulesettings'] = 'Settings';
+$string['nonewmodules'] = 'No newmodules';
+$string['pluginadministration'] = 'newmodule administration';
+$string['pluginname'] = 'WordPress Sync';
+$string['privacy:metadata'] = 'WordPress Sync does not store any personal data';
+$string['view'] = 'View';
+$string['wordpress_sync:addinstance'] = 'Add Instance of Mod WordPress Sync';
+$string['wordpress_sync:edit'] = 'Edit Instance of Mod WordPress Sync';
+$string['wordpress_sync:view'] = 'View Mod WordPress Sync';
